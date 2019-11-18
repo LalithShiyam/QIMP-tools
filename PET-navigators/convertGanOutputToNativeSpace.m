3fa01ba9edc1d552d41034700b7ab0178d25b06b
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2019-11-18 19:57:24 +0100

    Transforming gan output to native space

--- PET-navigators/convertGanOutputToNativeSpace.m
+++ PET-navigators/convertGanOutputToNativeSpace.m
@@ -15,17 +15,42 @@
 %------------------------------------------------------------------------%
 %                               Program start
 %------------------------------------------------------------------------%
-
+function []= convertGanOutputToNativeSpace(CGONinputs)
 % create the folder where the cropped images will be stored.
 
 cd(CGONinputs.where2Store)
-splitFiles=regexp(CGONinputs.path2Nifti,filesep,'split');
+splitFiles=regexp(CGONinputs.path2GanNifti,filesep,'split');
 convertedFolder=[splitFiles{end},'-','native'];
 mkdir(convertedFolder); % creating the converted folder for storing
-path2ConvFolder=[CNGinputs.where2Store,filesep,convertedFolder];
+path2ConvFolder=[CGONinputs.where2Store,filesep,convertedFolder];
 
 
 % load the nifti files.
 
 cd(CGONinputs.path2GanNifti);
 niftiFiles=dir('*.nii');
+cropRange=44;
+parfor lp=1:length(niftiFiles)
+    imgVol=niftiread(niftiFiles(lp).name);
+    xMax=344;
+    yMax=344;
+    zMax=127;
+    newVol{lp}=imgVol(:,:,1:zMax);
+end
+
+cd(CGONinputs.path2OrgNifti)
+orgNiftiFiles=dir('*.nii');
+parfor lp=1:length(orgNiftiFiles)
+    cd(CGONinputs.path2OrgNifti)
+    NativeFileName=['PET-Nav-',orgNiftiFiles(lp).name]; 
+    nativeVolumes=niftiread(orgNiftiFiles(lp).name);
+    nativeVolumes(cropRange+1:xMax-cropRange,cropRange+1:yMax-cropRange,:)=newVol{lp};
+    hdrInfo=niftiinfo(orgNiftiFiles(lp).name);
+    hdrInfo.Description='GAN derived PET navigators';
+    niftiwrite(nativeVolumes,NativeFileName,hdrInfo);
+    disp(['Writing ',NativeFileName,'...']);
+    movefile(NativeFileName,path2ConvFolder);
+end
+
+
+end
