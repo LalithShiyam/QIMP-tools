0525d049312b6217dce9617f40c07dee83657fda
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2020-03-17 18:05:24 +0100

    plotting the mid slice of a given medical series.

---
+++ PANDA/3D GAN/Matlab scripts/plotMedSeries.m
@@ -0,0 +1,30 @@
+function [] = plotMedSeries(pathOfNiftiSeries)
+
+% read the files in the folder
+
+cd(pathOfNiftiSeries)
+niftiFiles=dir('*nii');
+parfor lp=1:length(niftiFiles)
+    files2Sort{lp}=niftiFiles(lp).name;
+end
+sortedNiftiFiles=natsort(files2Sort);
+
+% Load the medical images 
+
+parfor lp=1:length(sortedNiftiFiles)
+     disp(['Reading ',sortedNiftiFiles{lp}])
+    img{lp}=niftiread(sortedNiftiFiles{lp});
+end
+clc 
+
+% create a plot 
+
+figure, 
+for lp=1:(length(img)-1)
+    subplot(6,6,lp)
+    midSlice=size(img{lp},3);
+    imshow(img{lp}(:,:,round(midSlice/2)),[])
+    title(['PET-Frame: ',num2str(lp)])
+end
+
+end
